86b171da744a2979566add116c7aacc0c3c99fa7
Al Vecchio <alvecchio@gmx.ch>
2023-08-27 22:18:03 +0200

    mariadb

--- app/__init__.py
+++ app/__init__.py
@@ -3,12 +3,15 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
 from config import Config
+from flask_wtf.csrf import CSRFProtect
 
 app = Flask(__name__)
 app.config.from_object(Config)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://admin:password@localhost/heroquest'
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 login = LoginManager(app)
 login.login_view = 'login'
+csrf = CSRFProtect(app)
 
 from app import routes, models
