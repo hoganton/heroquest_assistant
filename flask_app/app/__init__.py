2bffbde44e96feef77a3181513d46fe283e4842c
Al Vecchio <alvecchio@gmx.ch>
2023-09-03 19:11:44 +0000

    little fixes (collation, TZ etc.)

--- flask_app/app/__init__.py
+++ flask_app/app/__init__.py
@@ -7,7 +7,7 @@ from flask_wtf.csrf import CSRFProtect
 
 app = Flask(__name__)
 app.config.from_object(Config)
-#app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://admin:password@dbserver/heroquest'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://admin:password@mariadb/heroquest?charset=utf8mb4&collation=utf8mb4_bin'
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 login = LoginManager(app)
